5776016e36170a41fa73e081417944d3880f26d1
j-hutchison <jhutchison@outlook.de>
2022-09-29 10:31:42 +0200

    Reading image URLs from firestore in realtime and displaying in realtime

--- src/App.js
+++ src/App.js
@@ -1,15 +1,72 @@
+import React, { useEffect, useState } from "react";
+
 import "./App.css";
+
 import Banner from "./components/Banner";
 import Gallery from "./components/Gallery";
 import Title from "./components/Title";
 
+import { initializeApp } from "firebase/app";
+import { getStorage, ref } from "firebase/storage";
+import {
+	getFirestore,
+	collection,
+	query,
+	onSnapshot,
+} from "firebase/firestore";
+
 function App() {
+	const firebaseConfig = {
+		apiKey: "AIzaSyCbRDUXRb5H0NJvVblWasDrI96z01cHRMI",
+		authDomain: "firegram-bc599.firebaseapp.com",
+		projectId: "firegram-bc599",
+		storageBucket: "firegram-bc599.appspot.com",
+		messagingSenderId: "603616528372",
+		appId: "1:603616528372:web:21d40af8402bf159ae1ba1",
+	};
+
+	// Initialize Firebase
+	const app = initializeApp(firebaseConfig);
+	const [imageURLs, setImageURLs] = useState([]);
+
+	// Firestore DB used to provide a realtime connection to a database incl. the firebase storage URLs so that UI is dynamically updated
+	const fireStoredb = getFirestore(app);
+
+	// Firebase storage used for uploading images and returning download URLs for displaying images
+	const storage = getStorage(app);
+
+	useEffect(() => {
+		// gets images from the images collection on firestore
+		// async function getImages() {
+		// 	return await getDocs(collection(fireStoredb, "images"));
+		// }
+		// getImages()
+		// 	.then((docs) => {
+		// 		docs.forEach((doc) => {
+		// 			imageArray.push(doc.data().imageURL);
+		// 		});
+		// 	})
+		// 	.then(() => {
+		// 		setImageURLs(() => imageArray);
+		// 	});
+
+		// listen to firestore images collection in realtime and update imageURL state
+		const q = query(collection(fireStoredb, "images"));
+		const unsubscribe = onSnapshot(q, (querySnapshot) => {
+			const imageArray = [];
+			querySnapshot.forEach((doc) => {
+				imageArray.push(doc.data().imageURL);
+			});
+			setImageURLs(() => imageArray);
+		});
+	}, [fireStoredb]);
+
 	return (
 		<div className="App">
 			<Banner />
 			<main>
 				<Title />
-				<Gallery />
+				<Gallery imageURLs={imageURLs} storage={storage} />
 			</main>
 		</div>
 	);
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,34 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import classes from "./Gallery.module.css";
 
-const Gallery = () => {
+import { ref, listAll, getDownloadURL } from "firebase/storage";
+
+const Gallery = (props) => {
+	const [images, setImages] = useState([]);
+	const storageRef = ref(props.storage);
+
+	// useEffect(() => {
+	// 	listAll(storageRef)
+	// 		.then(async (res) => {
+	// 			const { items } = res;
+	// 			const urls = await Promise.all(
+	// 				items.map((item) => getDownloadURL(item))
+	// 			);
+
+	// 			setImages(urls);
+	// 			console.log(urls);
+	// 		})
+	// 		.catch((error) => {
+	// 			// Uh-oh, an error occurred!
+	// 		});
+	// }, []);
+
 	return (
 		<div className={classes["firegram-gallery"]}>
-			<img
-				className={classes["gallery-image"]}
-				src="https://cdn.pixabay.com/photo/2016/03/04/19/36/beach-1236581_960_720.jpg"
-				alt="holidays"
-			/>
-			<img
-				className={classes["gallery-image"]}
-				src="https://cdn.pixabay.com/photo/2020/07/24/16/37/cactus-5434469_960_720.jpg"
-				alt="cactus"
-			/>
-			<img
-				className={classes["gallery-image"]}
-				src="https://cdn.pixabay.com/photo/2016/04/13/07/18/blueberries-1326154_960_720.jpg"
-				alt="blueberries"
-			/>
-			<img
-				className={classes["gallery-image"]}
-				src="https://cdn.pixabay.com/photo/2016/08/15/10/14/wedding-1594957_960_720.jpg"
-				alt="wedding"
-			/>
-			<img
-				className={classes["gallery-image"]}
-				src="https://cdn.pixabay.com/photo/2015/05/31/14/23/organizer-791939_960_720.jpg"
-				alt="workstation"
-			/>
+			{props.imageURLs.map((url, index) => {
+				return (
+					<img
+						key={index}
+						className={classes["gallery-image"]}
+						src={url}
+						alt="image"
+					/>
+				);
+			})}
 		</div>
 	);
 };
